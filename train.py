537f2e057e7d3cb0796be6b439590fde3d0c486f
syedfarazhus <74476217+syedfarazhus@users.noreply.github.com>
2025-12-11 20:10:37 -0500

    move train file to root

---
+++ train.py
@@ -0,0 +1,9 @@
+print("TOP OF FILE: train.py is running")
+
+def main():
+    print("INSIDE main()")
+
+if __name__ == "__main__":
+    print("INSIDE __main__ guard, about to call main()")
+    main()
+
